2c3fe0cae60279aea38a2995c22e5732855a9ecb
mitch <mitch.c.dominici@gmail.com>
2024-12-09 20:37:44 -0600

    v2 - adds palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/*/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#0871e4',
+        'neutral-gray': '#bbb3b3',
+        'secondary-yellow': '#da9c4d',
+        'earthy-brown': '#6f430e',
+        'muted-gold': '#cf9c67',
+        'soft-sky-blue': '#c7eaf3',
+        'steel-blue': '#7087bb',
+        'soft-periwinkle': '#adb4e1',
+        'warm-taupe': '#977b5d',
+        'blush-beige': '#e4c8b4',
+      },
+    },
   },
   plugins: [],
 };
